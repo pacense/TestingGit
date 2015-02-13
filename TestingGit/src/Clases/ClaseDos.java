52aabad4828739996a79ad4f46726fc3a6d875f0
marcofernandezheras <marcofernandezheras@gmail.com>
2015-02-13 08:20:08 +0100

    ClaseDos:propiedadUno

--- TestingGit/src/Clases/ClaseDos.java
+++ TestingGit/src/Clases/ClaseDos.java
@@ -1,7 +1,19 @@
 package Clases;
 
 public class ClaseDos {
+	
+	int propiedadUno;
+	
+
 	public ClaseDos() {
 		System.out.println("Creando ClaseDos...");
+	}
+	
+	public int getPropiedadUno() {
+		return propiedadUno;
+	}
+	
+	public void setPropiedadUno(int propiedadUno) {
+		this.propiedadUno = propiedadUno;
 	}
 }
